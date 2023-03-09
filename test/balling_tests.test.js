f7ac5850c69901a71ef2c96ed98d0611a1a1ff40
Nkunzi <saphonkunzi@gmail.com>
2023-03-09 15:57:37 +0200

    (feat): Winning!

--- test/balling_tests.test.js
+++ test/balling_tests.test.js
@@ -9,34 +9,29 @@ describe('Sapho Nkunzi Bowling Game', function(){
         ballingAlley.PlayGame('');
         assert.equal(0,ballingAlley.GetScore());
     });
-
     //second bowl
     it('The player should be able to able to get 10 points if they hit a Spare', function(){
         ballingAlley.PlayGame('/');
         assert.equal(10, ballingAlley.GetScore());
     });
-    
     //third bowl
     it('The player should be able to increase their score to 20 points if they hit a Strike', function(){
         ballingAlley.PlayGame('X');
         assert.equal(20, ballingAlley.GetScore());
     });
-
     //fourth bowl
     it('The player should be able to increase their score to 31 from the previous total received on the Strike', function(){
         ballingAlley.PlayGame('15');
         assert.equal(31, ballingAlley.GetScore());
-
     })
+    //Bowl fifth - seventh
     it('The player should be able to increase their score to 42 from the previous total received on the Strike', function(){
         ballingAlley.PlayGame('15 21 20');
         assert.equal(42, ballingAlley.GetScore());
-
     })
-
+    //Bowl 8 display in counter
     it('The player should be able to see the number of swings they have made in the bowling alley, which is 8 in this case',function(){
         ballingAlley.PlayGame('13');
         assert.equal(8, ballingAlley.GetCounter())
-
     })
 })
