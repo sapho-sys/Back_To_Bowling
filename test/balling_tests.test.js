f4af3121fb78a2fd5cdb224a72cd4e47608807ef
Nkunzi <saphonkunzi@gmail.com>
2023-03-09 10:36:22 +0200

    (feat): ready for build

---
+++ test/balling_tests.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const BallingGame = require('../controller/balling_factory');
+
+describe('Balling Game', function(){
+    const balling = BallingGame();
+    it('The player should be able to make an unsuccessful roll & earn 0 in points', function(){
+
+    })
+
+    it('The player should be able to make a successful & earn at least 2 points', function(){
+
+    })
+
+    it('The player should be able to see their scores for each roll they made & also see their Total score for their overall game',function(){
+        
+    })
+})
