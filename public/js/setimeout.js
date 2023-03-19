20a835876d183a9519a2b469c59883fde6caab85
Nkunzi <saphonkunzi@gmail.com>
2023-03-19 08:09:36 +0200

    (feat): Pins down working

--- public/js/setimeout.js
+++ public/js/setimeout.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 	let errorMsg = document.querySelector('.errors');
+	
 	if (errorMsg.innerHTML !== '') {
 		setTimeout(function() {
 			errorMsg.innerHTML = '';
